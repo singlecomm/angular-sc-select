fc0981d21a9ffb99a32de86574d41345110c5f2c
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-09-02 17:53:22 +0100

    Initial scaffolding for the wrapper directive

--- src/angular-sc-select.js
+++ src/angular-sc-select.js
@@ -2,24 +2,103 @@ import angular from 'angular';
 import ngSanitize from 'angular-sanitize';
 import uiSelect from 'exports?"ui.select"!ui-select';
 import 'ui-select/dist/select.css';
-import selectPaginatorTemplate from './templates/select-paginator.html';
 
 export default angular
   .module('sc.select', [
     ngSanitize,
     uiSelect
   ])
+  .factory('scSelectParser', ['$parse', function($parse) {
+
+    const TYPEAHEAD_REGEXP = /^\s*([\s\S]+?)(?:\s+as\s+([\s\S]+?))?\s+for\s+(?:([\$\w][\$\w\d]*))\s+in\s+([\s\S]+?)$/;
+
+    return {
+      parse: function(input) {
+        var match = input.match(TYPEAHEAD_REGEXP);
+        if (!match) {
+          throw new Error(
+            'Expected typeahead specification in form of "_modelValue_ (as _label_)? for _item_ in _collection_"' +
+            ' but got "' + input + '".');
+        }
+
+        return {
+          itemName: match[3],
+          source: $parse(match[4]),
+          viewMapper: $parse(match[2] || match[1]),
+          modelMapper: $parse(match[1])
+        };
+      }
+    };
+
+  }])
+  .directive('scSelect', () => {
+
+    return {
+      restrict: 'E',
+      controller: function() {
+
+        var vm = this;
+        vm.currentPage = 1;
+        vm.changePage = function(newPage) {
+          vm.currentPage = newPage;
+        };
+
+      },
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+        pageLimit: '=',
+        totalItems: '='
+      }
+    };
+
+  })
+  .directive('scOptions', () => {
+
+    return {
+      restrict: 'A',
+      require: 'scSelect',
+      link: (scope, elm, attrs, scSelectCtrl) => {
+        scSelectCtrl.optionScope = scope; //this is the parent scope
+      }
+    };
+
+  })
   .directive('scSelectPaginator', () => {
 
     return {
       restrict: 'E',
-      template: selectPaginatorTemplate,
+      require: '?^scSelect',
+      template: `
+        <div style="padding: 10px 0;" ng-if="vm.scSelectCtrl">
+          <div class="btn-group">
+            <button
+             class="btn btn-default btn-xs"
+             ng-click="vm.scSelectCtrl.changePage(vm.scSelectCtrl.currentPage - 1)"
+             ng-disabled="vm.scSelectCtrl.currentPage <= 1">
+              <i class="glyphicon glyphicon-arrow-left"></i> Prev
+            </button>
+            <button
+            class="btn btn-default btn-xs"
+            ng-click="vm.scSelectCtrl.changePage(vm.scSelectCtrl.currentPage + 1)"
+            ng-disabled="vm.scSelectCtrl.currentPage >= (vm.scSelectCtrl.totalItems / vm.scSelectCtrl.pageLimit)">
+              Next <i class="glyphicon glyphicon-arrow-right"></i>
+            </button>
+          </div>
+          <small class="pull-right">
+            {{ (vm.scSelectCtrl.currentPage - 1) * vm.scSelectCtrl.pageLimit + 1 }} -
+            {{ vm.scSelectCtrl.currentPage * vm.scSelectCtrl.pageLimit > vm.scSelectCtrl.totalItems ? vm.scSelectCtrl.totalItems : vm.scSelectCtrl.currentPage * vm.scSelectCtrl.pageLimit }} of
+            {{ vm.scSelectCtrl.totalItems }} results
+           </small>
+        </div>
+      `,
       scope: {},
       bindToController: true,
-      controller: () => {
-
-      },
-      controllerAs: 'vm'
+      controller: angular.noop,
+      controllerAs: 'vm',
+      link: (scope, elm, attrs, scSelectCtrl) => {
+        scope.vm.scSelectCtrl = scSelectCtrl;
+      }
     };
 
   })
@@ -31,6 +110,7 @@ export default angular
       .find('ul')
       .next()
       .prepend(angular.element('<sc-select-paginator></sc-select-paginator>'));
+    multiTemplate.find('input').attr('ng-disabled', '$select.disabled || ($select.searchEnabled === false && $select.open)');
     $templateCache.put(multiTemplateName, multiTemplate.html());
 
   })
