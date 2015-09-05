60c2269d4d6d503fa355cc5da821f67f1a93b86a
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-09-05 16:34:11 +0100

    Remove es6 from demo ctrl in this branch

--- demo.js
+++ demo.js
@@ -1,14 +1,6 @@
-import angular from 'angular';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css';
-import scSelect from './src/module';
-import 'angular-sanitize';
-import 'ui-select';
-import 'ui-select/dist/select.css';
-
 angular
-  .module('demo', [scSelect])
-  .controller('DemoCtrl', /*@ngInject*/ function($http) {
+  .module('demo', ['sc.select'])
+  .controller('DemoCtrl', function($http) {
 
     var vm = this;
     vm.people = [
