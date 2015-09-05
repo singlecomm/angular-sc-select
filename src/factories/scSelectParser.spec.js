4683a07376633876688382f5fa330bfd6812cf68
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-09-05 18:23:03 +0100

    Add unit tests for select parser

--- src/factories/scSelectParser.spec.js
+++ src/factories/scSelectParser.spec.js
@@ -1,7 +1,101 @@
+import angular from 'angular';
+import 'angular-sanitize';
+import 'ui-select';
+
 describe('scSelectParser', () => {
 
-  it('should be a test', () => {
-    expect(true).to.be.true;
+  let scSelectParser;
+
+  beforeEach(angular.mock.module('sc.select'));
+
+  beforeEach(inject((_scSelectParser_) => {
+    scSelectParser = _scSelectParser_;
+  }));
+
+  describe('parse success', () => {
+
+    let scope, locals, actualResult, expectedResult, states, statesObj;
+
+    beforeEach(function() {
+      states = ['Alabama', 'California', 'Delaware'];
+      statesObj = [
+        {code: 'AL', name: 'Alabama'},
+        {code: 'CA', name: 'California'},
+        {code: 'DE', name: 'Delaware'}
+      ];
+    });
+
+    it('should parse the simplest array-based syntax', () => {
+      scope = {states: states};
+      actualResult = scSelectParser.parse('state for state in states');
+      locals = {
+        [actualResult.itemName]: 'Alabama'
+      };
+      expectedResult = {
+        source: states,
+        viewMapper: 'Alabama',
+        modelMapper: 'Alabama'
+      };
+    });
+
+    it('should parse the simplest function-based syntax', () => {
+      scope = {getStates: () => states};
+      actualResult = scSelectParser.parse('state for state in getStates()');
+      locals = {
+        [actualResult.itemName]: 'Alabama'
+      };
+      expectedResult = {
+        source: states,
+        viewMapper: 'Alabama',
+        modelMapper: 'Alabama'
+      };
+    });
+
+    it('should allow to specify custom model mapping that is used as a label as well', () => {
+      scope.states = statesObj;
+      actualResult = scSelectParser.parse('state.name for state in states');
+      locals = {
+        [actualResult.itemName]: {
+          name: 'Alabama'
+        }
+      };
+      expectedResult = {
+        source: statesObj,
+        viewMapper: 'Alabama',
+        modelMapper: 'Alabama'
+      };
+    });
+
+    it('should allow to specify custom view and model mappers', () => {
+      scope.states = statesObj;
+      actualResult = scSelectParser.parse('state.code as state.name + " ("+state.code+")" for state in states');
+      locals = {
+        [actualResult.itemName]: {
+          name: 'Alabama',
+          code: 'AL'
+        }
+      };
+      expectedResult = {
+        source: statesObj,
+        viewMapper: 'Alabama (AL)',
+        modelMapper: 'AL'
+      };
+    });
+
+    afterEach(() => {
+      expect(actualResult.source(scope)).to.eql(expectedResult.source);
+      expect(actualResult.viewMapper(scope, locals)).to.equal(expectedResult.viewMapper);
+      expect(actualResult.modelMapper(scope, locals)).to.equal(expectedResult.modelMapper);
+    });
+
+  });
+
+  describe('parse error', () => {
+
+    it('should throw a parse error', () => {
+      expect(scSelectParser.parse.bind(null, 'this is invalid')).to.throw(Error);
+    });
+
   });
 
 });
