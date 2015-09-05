6349bb0b82846cfd94d02bb8ab01e28c0b21c0c4
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-09-05 15:27:12 +0100

    Fix mimetype

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       exclude: /node_modules/
     }, {
       test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: 'url-loader?limit=10000&minetype=application/font-woff'
+      loader: 'url-loader?limit=10000&mimetype=application/font-woff'
     }, {
       test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
       loader: 'file-loader'
