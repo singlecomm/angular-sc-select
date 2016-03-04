9eb20258b4050d6d835947132131130e24704fe2
AymanJitan <ayman.jitan@gmail.com>
2016-03-04 09:33:20 -0900

    View model not updating - fix (#3)
    commit 37199bf2cadd99e2d45dbdbad979bc67b2398da1
    Author: Michael Blakeney <mike@acts18tech.com>
    Date:   Fri Mar 4 09:33:09 2016 -0900
    
        dep version bumps
    
    commit 3da0305dd9309d63082ff3585c0f468593630802
    Author: AymanJitan <ayman.jitan@gmail.com>
    Date:   Tue Feb 16 15:02:22 2016 -0500
    
        #2 build
    
    commit fdec1ac72848e1a4525471bf0f0852906244a712
    Author: AymanJitan <ayman.jitan@gmail.com>
    Date:   Tue Feb 16 14:37:47 2016 -0500
    
        #2 fix multiple option binding issues and update demo
    
    commit f133e53d734eea60eb98dd9bc683966901fd81f0
    Author: AymanJitan <ayman.jitan@gmail.com>
    Date:   Fri Jan 29 16:59:13 2016 -0500
    
        #2 update matchingItems function
    
    commit a8381cd8b9090391fcf8c8d8d6c5b962ff31d950
    Author: AymanJitan <ayman.jitan@gmail.com>
    Date:   Fri Jan 29 12:16:08 2016 -0500
    
        #2 Build
    
    commit 5b287885523acacbcf5ae35b4846730809b04749
    Author: AymanJitan <ayman.jitan@gmail.com>
    Date:   Fri Jan 29 12:09:15 2016 -0500
    
        #2 Upgrade to angular 1.4.9 and fix controller model compatibility issues

--- src/directives/scSelect.js
+++ src/directives/scSelect.js
@@ -109,24 +109,50 @@ export default function scSelect() {
         vm.ngModelCtrl = ngModelCtrl;
 
         ngModelCtrl.$render = function() {
-
           if (!ngModelCtrl.$viewValue) {
             return;
           }
-
-          const matchingItems = vm.items.filter((item) => {
-            const itemValue = vm.parsedOptions.modelMapper({
-              [vm.parsedOptions.itemName]: item
-            });
-            if (vm.multiple) {
-              if (!ngModelCtrl.$viewValue) {
-                return false;
+          //var cc = vm.multiple ? ngModelCtrl.$viewValue : vm.items;
+          var items;
+          if (vm.multiple) {
+            items = ngModelCtrl.$viewValue;
+          } else {
+            items = vm.items;
+          }
+          var matchingItems = [];
+          if (angular.isArray(items)) {
+            matchingItems = items.filter((item) => {
+              var itemValue;
+              if (!angular.isArray(item)) {
+                itemValue = item;
+              } else {
+                itemValue = vm.parsedOptions.modelMapper({
+                  [vm.parsedOptions.itemName]: item
+                });
               }
-              return ngModelCtrl.$viewValue.indexOf(itemValue) > -1;
-            } else {
-              return ngModelCtrl.$viewValue === itemValue;
-            }
-          });
+              if (vm.multiple) {
+                var found = false;
+                if (typeof ngModelCtrl.$viewValue === 'object') {
+
+                  angular.forEach(ngModelCtrl.$viewValue, function(val) {
+                    if (typeof val === 'string') {
+                      if (val.indexOf(itemValue) > -1) {
+                        found = true;
+                      }
+                    }
+                    if (val && itemValue && val.id === itemValue.id) {
+                      found = true;
+                    }
+                  });
+                } else if (ngModelCtrl.$viewValue.indexOf(itemValue) > -1) {
+                  found = true;
+                }
+                return found;
+              } else {
+                return ngModelCtrl.$viewValue === itemValue;
+              }
+            });
+          }
           if (vm.multiple) {
             vm.selected = matchingItems;
           } else {
@@ -205,7 +231,7 @@ export default function scSelect() {
       pageLimit: '=',
       totalItems: '=',
       placeholder: '@',
-      multiple: '=',
+      multiple: '=?',
       ngDisabled: '=',
       searchEnabled: '=',
       refreshDelay: '=',
