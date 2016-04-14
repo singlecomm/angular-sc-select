aeb8884cd4d9dcfeba164f2f5c38c50b4656c304
Jay Thomas <jay@gfax.ch>
2016-04-14 17:47:26 -0400

    Extract in-line scSelect controller

--- src/directives/scSelect.js
+++ src/directives/scSelect.js
@@ -49,192 +49,7 @@ export default function scSelect() {
     restrict: 'E',
     require: 'ngModel',
     template: '<div></div>',
-    controller: function($attrs, $element, $compile, $parse, $scope, $q, $timeout, scSelectParser) {
-
-      'ngInject';
-
-      const vm = this;
-      const loadingDelay = angular.isDefined(vm.loadingDelay) ? vm.loadingDelay : 0;
-      vm.currentPage = 1;
-      vm.canToggleAll = vm.multiple && !vm.pageLimit;
-
-      const selectElm = angular.element(template);
-      if (vm.multiple) {
-        selectElm.find('ui-select').attr('multiple', 'multiple');
-      }
-      $compile(selectElm)($scope);
-      $element.append(selectElm);
-
-      vm.items = [];
-
-      let oldSearchText;
-
-      vm.searchItems = function() {
-        if (vm.uiSelectCtrl) {
-          //reset page if the search text has changed
-          if (oldSearchText !== vm.uiSelectCtrl.search) {
-            vm.currentPage = 1;
-          }
-
-          oldSearchText = vm.uiSelectCtrl.search;
-          const setLoadingTimeout = $timeout(() => {
-            vm.loading = true;
-            vm.items = [];
-          }, loadingDelay);
-
-          return $q.when(vm.parsedOptions.source(vm.optionScope, {
-            page: vm.currentPage,
-            searchText: vm.uiSelectCtrl.search
-          })).then((items) => {
-            vm.items = items;
-          }).finally(() => {
-            vm.loading = false;
-            $timeout.cancel(setLoadingTimeout);
-          });
-
-        }
-      };
-
-      vm.changePage = function(newPage) {
-        vm.currentPage = newPage;
-        vm.searchItems();
-      };
-
-      vm.parsedOptions = scSelectParser.parse($attrs.scOptions);
-
-      vm.setOptionScope = function(scope) {
-        vm.optionScope = scope; //expose for testing
-        vm.changePage(vm.currentPage);
-      };
-
-      vm.setNgModelCtrl = function(ngModelCtrl) {
-        vm.ngModelCtrl = ngModelCtrl;
-
-        ngModelCtrl.$render = function() {
-          if (!ngModelCtrl.$viewValue) {
-            return;
-          }
-          //var cc = vm.multiple ? ngModelCtrl.$viewValue : vm.items;
-          var items;
-          if (vm.multiple) {
-            items = ngModelCtrl.$viewValue;
-          } else {
-            items = vm.items;
-          }
-          var matchingItems = [];
-          if (angular.isArray(items)) {
-            matchingItems = items.filter((item) => {
-              var itemValue;
-              if (!angular.isArray(item)) {
-                itemValue = item;
-              } else {
-                itemValue = vm.parsedOptions.modelMapper({
-                  [vm.parsedOptions.itemName]: item
-                });
-              }
-              if (vm.multiple) {
-                var found = false;
-                if (typeof ngModelCtrl.$viewValue === 'object') {
-
-                  angular.forEach(ngModelCtrl.$viewValue, function(val) {
-                    if (typeof val === 'string') {
-                      if (val.indexOf(itemValue) > -1) {
-                        found = true;
-                      }
-                    }
-                    if (val && itemValue && val.id === itemValue.id) {
-                      found = true;
-                    }
-                  });
-                } else if (ngModelCtrl.$viewValue.indexOf(itemValue) > -1) {
-                  found = true;
-                }
-                return found;
-              } else {
-                if (typeof itemValue === 'object') {
-                  return ngModelCtrl.$viewValue === itemValue.id || ngModelCtrl.$viewValue === itemValue.label || ngModelCtrl.$viewValue.id === itemValue.id;
-                }
-                return ngModelCtrl.$viewValue === itemValue;
-              }
-            });
-          }
-
-          if (matchingItems.length === 0 && typeof ngModelCtrl.$viewValue === 'object') {
-            matchingItems = [ngModelCtrl.$viewValue];
-          }
-
-          if (vm.multiple) {
-            vm.selected = matchingItems;
-          } else {
-            vm.selected = matchingItems[0];
-          }
-        };
-
-        //Ensure items array has been populated first - as it uses $q.when it will be after the first digest
-        $timeout(function() {
-          vm.ngModelCtrl.$render();
-        });
-
-      };
-
-      vm.modelChanged = function() {
-        let modelValue;
-        if (vm.multiple) {
-          modelValue = vm.selected.map((item) => {
-            return vm.parsedOptions.modelMapper({
-              [vm.parsedOptions.itemName]: item
-            });
-          });
-        } else {
-          modelValue = vm.parsedOptions.modelMapper({
-            [vm.parsedOptions.itemName]: vm.selected
-          });
-        }
-        vm.ngModelCtrl.$setViewValue(modelValue);
-      };
-
-      vm.getMappedItem = function(localItem) {
-        return vm.parsedOptions.viewMapper({
-          [vm.parsedOptions.itemName]: localItem
-        });
-      };
-
-      vm.selectAll = function() {
-        vm.selected = vm.items;
-        vm.modelChanged();
-      };
-
-      vm.deselectAll = function() {
-        vm.selected = [];
-        vm.modelChanged();
-      };
-
-      $scope.$watch(function() {
-        return vm.ngModelCtrl.$modelValue;
-      }, function(selected) {
-        if (!selected) {
-          vm.selected = vm.multiple ? [] : '';
-        }
-      }, true);
-
-      if (!angular.isDefined(vm.pageLimit)) {
-        $scope.$watch(function() {
-          return vm.parsedOptions.source(vm.optionScope);
-        }, function(items) {
-          if (items) {
-            vm.items = items;
-          }
-        });
-      }
-
-      if (angular.isUndefined(vm.groupBy) && angular.isDefined($attrs.groupBy)) {
-        const getGroupBy = $parse($attrs.groupBy);
-        vm.groupBy = function(item) {
-          return getGroupBy(item);
-        };
-      }
-
-    },
+    controller: controller,
     controllerAs: 'vm',
     bindToController: true,
     scope: {
@@ -252,5 +67,192 @@ export default function scSelect() {
       scope.vm.setNgModelCtrl(ngModelCtrl);
     }
   };
+
+  function controller($attrs, $element, $compile, $parse, $scope, $q, $timeout, scSelectParser) {
+
+    'ngInject';
+
+    const vm = this;
+    const loadingDelay = angular.isDefined(vm.loadingDelay) ? vm.loadingDelay : 0;
+    vm.currentPage = 1;
+    vm.canToggleAll = vm.multiple && !vm.pageLimit;
+
+    const selectElm = angular.element(template);
+    if (vm.multiple) {
+      selectElm.find('ui-select').attr('multiple', 'multiple');
+    }
+    $compile(selectElm)($scope);
+    $element.append(selectElm);
+
+    vm.items = [];
+
+    let oldSearchText;
+
+    vm.searchItems = function() {
+      if (vm.uiSelectCtrl) {
+        //reset page if the search text has changed
+        if (oldSearchText !== vm.uiSelectCtrl.search) {
+          vm.currentPage = 1;
+        }
+
+        oldSearchText = vm.uiSelectCtrl.search;
+        const setLoadingTimeout = $timeout(() => {
+          vm.loading = true;
+          vm.items = [];
+        }, loadingDelay);
+
+        return $q.when(vm.parsedOptions.source(vm.optionScope, {
+          page: vm.currentPage,
+          searchText: vm.uiSelectCtrl.search
+        })).then((items) => {
+          vm.items = items;
+        }).finally(() => {
+          vm.loading = false;
+          $timeout.cancel(setLoadingTimeout);
+        });
+
+      }
+    };
+
+    vm.changePage = function(newPage) {
+      vm.currentPage = newPage;
+      vm.searchItems();
+    };
+
+    vm.parsedOptions = scSelectParser.parse($attrs.scOptions);
+
+    vm.setOptionScope = function(scope) {
+      vm.optionScope = scope; //expose for testing
+      vm.changePage(vm.currentPage);
+    };
+
+    vm.setNgModelCtrl = function(ngModelCtrl) {
+      vm.ngModelCtrl = ngModelCtrl;
+
+      ngModelCtrl.$render = function() {
+        if (!ngModelCtrl.$viewValue) {
+          return;
+        }
+        //var cc = vm.multiple ? ngModelCtrl.$viewValue : vm.items;
+        var items;
+        if (vm.multiple) {
+          items = ngModelCtrl.$viewValue;
+        } else {
+          items = vm.items;
+        }
+        var matchingItems = [];
+        if (angular.isArray(items)) {
+          matchingItems = items.filter((item) => {
+            var itemValue;
+            if (!angular.isArray(item)) {
+              itemValue = item;
+            } else {
+              itemValue = vm.parsedOptions.modelMapper({
+                [vm.parsedOptions.itemName]: item
+              });
+            }
+            if (vm.multiple) {
+              var found = false;
+              if (typeof ngModelCtrl.$viewValue === 'object') {
+
+                angular.forEach(ngModelCtrl.$viewValue, function(val) {
+                  if (typeof val === 'string') {
+                    if (val.indexOf(itemValue) > -1) {
+                      found = true;
+                    }
+                  }
+                  if (val && itemValue && val.id === itemValue.id) {
+                    found = true;
+                  }
+                });
+              } else if (ngModelCtrl.$viewValue.indexOf(itemValue) > -1) {
+                found = true;
+              }
+              return found;
+            } else {
+              if (typeof itemValue === 'object') {
+                return ngModelCtrl.$viewValue === itemValue.id || ngModelCtrl.$viewValue === itemValue.label || ngModelCtrl.$viewValue.id === itemValue.id;
+              }
+              return ngModelCtrl.$viewValue === itemValue;
+            }
+          });
+        }
+
+        if (matchingItems.length === 0 && typeof ngModelCtrl.$viewValue === 'object') {
+          matchingItems = [ngModelCtrl.$viewValue];
+        }
+
+        if (vm.multiple) {
+          vm.selected = matchingItems;
+        } else {
+          vm.selected = matchingItems[0];
+        }
+      };
+
+      //Ensure items array has been populated first - as it uses $q.when it will be after the first digest
+      $timeout(function() {
+        vm.ngModelCtrl.$render();
+      });
+
+    };
+
+    vm.modelChanged = function() {
+      let modelValue;
+      if (vm.multiple) {
+        modelValue = vm.selected.map((item) => {
+          return vm.parsedOptions.modelMapper({
+            [vm.parsedOptions.itemName]: item
+          });
+        });
+      } else {
+        modelValue = vm.parsedOptions.modelMapper({
+          [vm.parsedOptions.itemName]: vm.selected
+        });
+      }
+      vm.ngModelCtrl.$setViewValue(modelValue);
+    };
+
+    vm.getMappedItem = function(localItem) {
+      return vm.parsedOptions.viewMapper({
+        [vm.parsedOptions.itemName]: localItem
+      });
+    };
+
+    vm.selectAll = function() {
+      vm.selected = vm.items;
+      vm.modelChanged();
+    };
+
+    vm.deselectAll = function() {
+      vm.selected = [];
+      vm.modelChanged();
+    };
+
+    $scope.$watch(function() {
+      return vm.ngModelCtrl.$modelValue;
+    }, function(selected) {
+      if (!selected) {
+        vm.selected = vm.multiple ? [] : '';
+      }
+    }, true);
+
+    if (!angular.isDefined(vm.pageLimit)) {
+      $scope.$watch(function() {
+        return vm.parsedOptions.source(vm.optionScope);
+      }, function(items) {
+        if (items) {
+          vm.items = items;
+        }
+      });
+    }
+
+    if (angular.isUndefined(vm.groupBy) && angular.isDefined($attrs.groupBy)) {
+      const getGroupBy = $parse($attrs.groupBy);
+      vm.groupBy = function(item) {
+        return getGroupBy(item);
+      };
+    }
+
+  }
 
 }
